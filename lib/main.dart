69d63c2a04e2b45248e76f0b7529bd5365636982
Anup Cowkur <anupcowkur89@gmail.com>
2020-11-10 14:23:16 +0530

    Add algorithm idea

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class RorschachApp extends StatelessWidget {
 }
 
 // Algorithm:
+// Idea 1:
 // Make blob class that has set of points and a set of mirror points.Randomly
 // generate these blobs at app start.
 //
@@ -28,6 +29,16 @@ class RorschachApp extends StatelessWidget {
 //
 // Have a button to restart the whole operation with new set of
 // randomly generated blobs.
+//
+// Idea 2:
+// Starting at center of widget, traverse from point to edge of circle one pixel
+// at time. Randomly decide whether to draw a random sized disk at each pixel.
+// If yes, draw disk and draw a mirror disk. Mirror point can be found using
+// the formula to find third vertex of an isosceles triangle since current point,
+// center of widget and mirror point form an isosceles triangle.
+// Once end is reach, rotate by 5 degrees and walk again from center to end
+// repeating the same. To animate, shift points by some positive and negative
+// value.
 class Rorschach extends StatefulWidget {
   @override
   _RorschachState createState() => _RorschachState();
